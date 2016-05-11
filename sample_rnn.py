f9c51063a57e4391df29125852c9b6a037a68668
Joao Felipe Santos <joao.eel@gmail.com>
2016-05-10 20:47:47 -0500

    Updated the sampling script to accept a seed input and added early support to Python 3

--- sample_rnn.py
+++ sample_rnn.py
@@ -1,18 +1,23 @@
+from __future__ import print_function
+
 import theano
 import lasagne
 import os
 import sys
 import time
 import importlib
-import cPickle as pickle
+if sys.version_info < (3,0):
+    import cPickle as pickle
+else:
+    import pickle
 import numpy as np
 import theano.tensor as T
 from lasagne.layers import *
 
 theano.config.floatX = 'float64'
 
-if not (2 <= len(sys.argv) <= 5):
-    sys.exit("Usage: sample_rnn.py <metadata_path> <rng_seed> [sampling temperature] [ntunes]")
+if not (2 <= len(sys.argv) <= 6):
+    sys.exit("Usage: sample_rnn.py <metadata_path> <rng_seed> [sampling temperature] [seed] [ntunes]")
 
 metadata_path = sys.argv[1]
 rng_seed = int(sys.argv[2])
@@ -21,7 +26,8 @@ rng_seed = int(sys.argv[2])
 # rng_seed = 42
 
 temperature = int(sys.argv[3]) if len(sys.argv) == 4 or len(sys.argv) == 5 else 1
-ntunes = int(sys.argv[4]) if len(sys.argv) == 5 else 64
+ntunes = int(sys.argv[5]) if len(sys.argv) == 6 else 64
+seed = sys.argv[4] if len(sys.argv) >= 5 else None
 
 with open(metadata_path) as f:
     metadata = pickle.load(f)
@@ -38,7 +44,7 @@ token2idx = metadata['token2idx']
 idx2token = dict((v, k) for k, v in token2idx.iteritems())
 vocab_size = len(token2idx)
 
-print 'Building the model'
+print('Building the model')
 x = T.imatrix('x')
 l_inp = InputLayer((1, None), input_var=x)
 
@@ -68,10 +74,10 @@ lasagne.layers.set_all_param_values(l_out, metadata['param_values'])
 
 all_layes = lasagne.layers.get_all_layers(l_out)
 for layer in all_layes:
-    print layer.__class__.__name__
+    print(layer.__class__.__name__)
     for p in layer.get_params():
-        print p.get_value().shape
-print 'number of parameters:', lasagne.layers.count_params(l_out)
+        print(p.get_value().shape)
+print('number of parameters: ', lasagne.layers.count_params(l_out))
 
 predict = theano.function([x], predictions)
 
@@ -81,12 +87,18 @@ rng = np.random.RandomState(rng_seed)
 vocab_idxs = np.arange(vocab_size)
 f = open(target_path, 'w')
 
+# Converting the seed passed as an argument into a list of idx
+seed_sequence = [start_idx]
+if seed is not None:
+    for token in seed.split(' '):
+        seed_sequence.append(token2idx[token])
+
 for i in xrange(ntunes):
-    sequence = [start_idx]
+    sequence = seed_sequence
     while sequence[-1] != end_idx:
         next_itoken = rng.choice(vocab_idxs, p=predict(np.array([sequence], dtype='int32')))
         sequence.append(next_itoken)
-    print i
+    print(i)
     abc_tune = [idx2token[i] for i in sequence[1:-1]]
     f.write(abc_tune[0] + '\n')
     f.write(abc_tune[1] + '\n')
