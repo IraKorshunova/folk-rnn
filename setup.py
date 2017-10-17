86459167d31b529a3d48e7ab1a26c14fa588054a
Toby Harris <operator@tobyz.net>
2017-10-17 14:14:11 +0100

    Fixing theano version to 0.7 due to depreciated function
    ```
    File "/Users/tobyz/Documents/python-venv/folkrnn/lib/python3.6/site-packages/lasagne/layers/pool.py", line 6, in <module>
        from theano.tensor.signal import downsample
    ImportError: cannot import name 'downsample'
    ```

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'theano',
+        'theano<0.8',
         'lasagne',
         ],
 
