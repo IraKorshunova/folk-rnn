51aaed81fe5a974bb6f27f5361624d7e13cf66b7
Toby Harris <operator@tobyz.net>
2017-10-18 12:36:37 +0100

    Python 2 only

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
     ],
 
     # What does your project relate to?
