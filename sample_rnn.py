381184a2d6659a47520cedd6d4dfa7bb1c5189f7
Toby Harris <operator@tobyz.net>
2017-10-18 18:00:54 +0100

    Folk_RNN class, refactored sample_rnn to use it
    This produces the same output as original sample_rnn
    ```
    python sample_rnn_new.py metadata/config5-wrepeats-20160112-222521.pkl
    M:4/4
    K:Cdor
    c 3 d c 2 B G | G F F 2 G B c d | c 3 d c B G A | B G G F G 2 f e | d 2 c d c B G A | B G F G B F G F | G B c d c d e f | g b f d e 2 e f | d B B 2 B 2 d c | B G G 2 B G F B | d B B 2 c d e f | g 2 f d g f d c | d g g 2 f d B c | d B B 2 B G B c | d f d c B 2 d B | c B B G F B G B | d 2 c d d 2 f d | g e c d e 2 f g | f d d B c 2 d B | d c c B G B B c | d 2 c d d 2 f d | c d c B c 2 d f | g b b 2 g a b d' | c' d' b g f d d f |
    ```

--- sample_rnn.py
+++ sample_rnn.py
@@ -1,7 +1,5 @@
 from __future__ import print_function
 
-import theano
-import lasagne
 import os
 import sys
 import time
@@ -10,12 +8,8 @@ if sys.version_info < (3,0):
     import cPickle as pickle
 else:
     import pickle
-import numpy as np
-import theano.tensor as T
-from lasagne.layers import *
 
-theano.config.floatX = 'float64'
-
+from folk_rnn import Folk_RNN
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -45,79 +39,21 @@ if not os.path.isdir('samples'):
 target_path = "samples/%s-s%d-%.2f-%s.txt" % (
     metadata['experiment_id'], rng_seed, temperature, time.strftime("%Y%m%d-%H%M%S", time.localtime()))
 
-token2idx = metadata['token2idx']
-idx2token = dict((v, k) for k, v in token2idx.iteritems())
-vocab_size = len(token2idx)
+if config.one_hot:
+    config.embedding_size = None
 
-print('Building the model')
-x = T.imatrix('x')
-
-l_inp = InputLayer((1, None), input_var=x)
-
-W_emb = np.eye(vocab_size, dtype='float32') if config.one_hot else lasagne.init.Orthogonal()
-emb_output_size = vocab_size if config.one_hot else config.embedding_size
-l_emb = EmbeddingLayer(l_inp, input_size=vocab_size, output_size=emb_output_size, W=W_emb)
-
-main_layers = []
-for _ in xrange(config.num_layers):
-    if not main_layers:
-        main_layers.append(LSTMLayer(l_emb, num_units=config.rnn_size,
-                                     peepholes=False,
-                                     grad_clipping=config.grad_clipping))
-    else:
-        main_layers.append(LSTMLayer(main_layers[-1], num_units=config.rnn_size,
-                                     peepholes=False,
-                                     grad_clipping=config.grad_clipping))
-    if config.dropout > 0:
-        main_layers.append(DropoutLayer(main_layers[-1], p=config.dropout))
-
-l_reshp = ReshapeLayer(main_layers[-1], (-1, config.rnn_size))
-l_out = DenseLayer(l_reshp, num_units=vocab_size, nonlinearity=lasagne.nonlinearities.identity)
-predictions = T.nnet.softmax(lasagne.layers.get_output(l_out, deterministic=True)[-1, :] / temperature)[0]
-
-all_params = lasagne.layers.get_all_params(l_out)
-lasagne.layers.set_all_param_values(l_out, metadata['param_values'])
-
-all_layes = lasagne.layers.get_all_layers(l_out)
-for layer in all_layes:
-    print(layer.__class__.__name__)
-    for p in layer.get_params():
-        print(p.get_value().shape)
-print('number of parameters: ', lasagne.layers.count_params(l_out))
-
-predict = theano.function([x], predictions)
-
-start_idx, end_idx = token2idx['<s>'], token2idx['</s>']
-
-rng = np.random.RandomState(rng_seed)
-vocab_idxs = np.arange(vocab_size)
-
-# Converting the seed passed as an argument into a list of idx
-seed_sequence = [start_idx]
-if seed is not None:
-    for token in seed.split(' '):
-        seed_sequence.append(token2idx[token])
-
+folk_rnn = Folk_RNN(
+    metadata['token2idx'],
+    metadata['param_values'], 
+    config.num_layers, 
+    config.rnn_size,
+    config.grad_clipping,
+    config.dropout, 
+    config.embedding_size, 
+    rng_seed, 
+    temperature
+    )
+folk_rnn.seed_tune(seed)
 for i in xrange(ntunes):
-    sequence = seed_sequence[:]
-    while sequence[-1] != end_idx:
-        next_itoken = rng.choice(vocab_idxs, p=predict(np.array([sequence], dtype='int32')))
-        sequence.append(next_itoken)
-
-    abc_tune = [idx2token[j] for j in sequence[1:-1]]
-    if not args.terminal:
-        f = open(target_path, 'a+')
-        f.write('X:' + repr(i) + '\n')
-        f.write(abc_tune[0] + '\n')
-        f.write(abc_tune[1] + '\n')
-        f.write(' '.join(abc_tune[2:]) + '\n\n')
-        f.close()
-    else:
-        print('X:' + repr(i))
-        print(abc_tune[0])
-        print(abc_tune[1])
-        print(' '.join(abc_tune[2:]) + '\n')
-
-
-if not args.terminal:
-    print('Saved to '+target_path)
+    print(folk_rnn.compose_tune())
+    
